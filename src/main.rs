f57a2b47d4c34fbb314506b3806e8b8070a8130f
Matt Stetter <matthew.stetter@gmail.com>
2022-01-27 20:09:07 -0500

    Wrote simple thread join example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::thread;
+use std::thread::JoinHandle;
+
 fn main() {
-    println!("Hello, world!");
+    let mut join_handles: Vec<JoinHandle<()>> = Vec::new();
+    for i in 0..10 {
+        join_handles.push(thread::spawn(move || {
+            println!("hello from thread #{}", i);
+        }));
+    }
+    for handle in join_handles {
+        handle.join().unwrap();
+    }
+    println!("Goodbye");
 }
