c9687ea36996f5da2ffe4ff8846264d90f83fd30
Matt Stetter <matthew.stetter@gmail.com>
2022-01-30 22:58:31 -0500

    Added avec macro

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 use std::thread;
 use std::thread::JoinHandle;
 
+#[macro_export]
+macro_rules! avec {
+    () => { Vec::new() };
+    ($($elem : expr),+) => {{
+        let mut new = Vec::new();
+        $(new.push($elem);)*
+        new
+    }};
+    ($elem : expr ; $n : expr) => {{
+        let mut new = Vec::new();
+        for _ in 0..$n {
+            new.push($elem);
+        }
+        new
+    }};
+}
+
 fn main() {
     let mut join_handles: Vec<JoinHandle<()>> = Vec::new();
     for i in 0..10 {
@@ -12,4 +29,10 @@ fn main() {
         handle.join().unwrap();
     }
     println!("Goodbye");
+    let x: Vec<i32> = avec![5];
+    dbg!(&x);
+    let x: Vec<i32> = avec![5; 5];
+    dbg!(&x);
+    let x = avec![1, 2, 3, 4, 5, 6];
+    dbg!(&x);
 }
